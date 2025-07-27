1c4988723dc27f01d0c554c4a157435aa3a252f9
mrnp-dev <2023304093@dhvsu.edu.ph>
2025-07-27 12:33:31 +0800

    Update App.jsx

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -15,9 +15,9 @@ const Part = (props) => {
 const Content = (props) => {
   return(
     <div>
-      <Part part={p1} exercises={e1}/>
-      <Part part={p2} exercises={e2}/>
-      <Part part={p3} exercises={e3}/>
+      <Part part={props.p1} exercises={props.e1}/>
+      <Part part={props.p2} exercises={props.e2}/>
+      <Part part={props.p3} exercises={props.e3}/>
     </div>
   )
 }
